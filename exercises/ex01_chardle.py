5c7001d5d9a4ea5195797ba1f5e59c641e43efb7
Vineeta Pasala <vpasala@unc.edu>
2023-09-10 23:01:43 -0400

    Progress to exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,16 +3,16 @@ __author__ = "730616700"
 
 word: str = input("Enter a 5-character word: ")
 if len(word) != 5:
-       print("Error: Word must contain 5 characters")
-       exit()
+    print("Error: Word must contain 5 characters")
+    exit()
 else:
-       single_char: str = input("Enter a single character: ")
+    single_char: str = input("Enter a single character: ")
        
 if len(single_char) != 1:
-       print("Error: Character must be a single character")
-       exit()
+    print("Error: Character must be a single character")
+    exit()
 else:
-       print("Searching for " + single_char + " in " + word)
+    print("Searching for " + single_char + " in " + word)
 
 index: int = 0
 char_count: int = 0
@@ -25,37 +25,37 @@ else:
     index = index + 1
     
 if word[index] == single_char:
-        print(single_char + " found at index " + str(index))
-        index = index + 1
-        char_count = char_count + 1
+    print(single_char + " found at index " + str(index))
+    index = index + 1
+    char_count = char_count + 1
 else:
-        index = index + 1
+    index = index + 1
     
 if word[index] == single_char:
-        print(single_char + " found at index " + str(index))
-        index = index + 1
-        char_count = char_count + 1
+    print(single_char + " found at index " + str(index))
+    index = index + 1
+    char_count = char_count + 1
 else:
-        index = index + 1
+    index = index + 1
 
 if word[index] == single_char:
-        print(single_char + " found at index " + str(index))
-        index = index + 1
-        char_count = char_count + 1
+    print(single_char + " found at index " + str(index))
+    index = index + 1
+    char_count = char_count + 1
 else:
-        index = index + 1
+    index = index + 1
 
 if word[index] == single_char:
-        print(single_char + " found at index " + str(index))
-        index = index + 1
-        char_count = char_count + 1
+    print(single_char + " found at index " + str(index))
+    index = index + 1
+    char_count = char_count + 1
 else:
-        index = index + 1
+    index = index + 1
 
 if char_count == 1:
-       print(str(char_count) + " instance of " + single_char + " found in " + word)
+    print(str(char_count) + " instance of " + single_char + " found in " + word)
 else:
-       if char_count != 0:
+    if char_count != 0:
               print(str(char_count) + " instances of " + single_char + " found in " + word)
-       else:
+    else:
               print("No instances of " + single_char + " found in " + word)
