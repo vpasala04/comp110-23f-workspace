42c94fd15ca9e9763aa3378f9594fd691fe9591c
Vineeta Pasala <vpasala@unc.edu>
2023-11-04 19:17:07 -0400

    update

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -4,18 +4,33 @@ __author__ = "730616700"
 
 
 def invert(input_dict: dict[str, str]) -> dict[str, str]:
+    """Inverts the keys and values of a list."""
     inverted_dict: dict[str, str] = {}
     for key in input_dict:
-        if key == inverted_dict[key]:
+        if input_dict[key] in inverted_dict:
             raise KeyError("Duplicate key in inverted_dict")
-        else:
-            inverted_dict[input_dict[key]] = key
+        inverted_dict[input_dict[key]] = key
+    return inverted_dict
 
 print(invert({'apple': 'cat'}))
 
-def favorite_colors(colors_dict: [str, str]) -> str:
-    color_count: dict = {}
-    for key, value in colors_dict.items():
-        if colors_dict[item] == colors_dict[item + 1]:
-            color = color + dict1[item]
-            item = item + 1
+def favorite_color(colors_dict: [str, str]) -> str:
+    """Returns which color appears most frequently."""
+    color_count: dict[str, int] = {}
+    max_count: int = 0
+    final_color: str = ""
+    for key in colors_dict:
+        if key in color_count:
+            color_count = color_count + 1
+        else:
+            color_count = 1
+    for key in color_count:
+        if color_count[key] > max_count:
+            max_count = color_count[key]
+            final_color = key
+    return final_color
+
+print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
+        
+
+
