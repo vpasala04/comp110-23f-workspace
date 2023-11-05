e0a104d094b78307982ac6ce320ae4ea3854ab32
Vineeta Pasala <vpasala@unc.edu>
2023-11-05 01:31:11 -0400

    ex06

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -12,7 +12,6 @@ def invert(input_dict: dict[str, str]) -> dict[str, str]:
         inverted_dict[input_dict[key]] = key
     return inverted_dict
 
-print(invert({'apple': 'cat'}))
 
 def favorite_color(colors_dict: [str, str]) -> str:
     """Returns which color appears most frequently."""
@@ -20,17 +19,46 @@ def favorite_color(colors_dict: [str, str]) -> str:
     max_count: int = 0
     final_color: str = ""
     for key in colors_dict:
-        if key in color_count:
-            color_count = color_count + 1
+        color = colors_dict[key]
+        if color in color_count:
+            color_count[color] = color_count[color] + 1
         else:
-            color_count = 1
-    for key in color_count:
-        if color_count[key] > max_count:
-            max_count = color_count[key]
-            final_color = key
+            color_count[color] = 1
+    for color in color_count:
+        if color_count[color] > max_count:
+            max_count = color_count[color]
+            final_color = color
     return final_color
 
-print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
-        
+
+def count(input_list: list[str]) -> dict[str, int]:
+    """Returns a dictionary mapping list values to their frequency counts."""
+    final_dict: dict = {}
+    count_value: int = 0
+    for key in input_list:
+        if key in final_dict:
+            final_dict[key] = final_dict[key] + 1
+        else:
+            final_dict[key] = 1
+    return final_dict
 
 
+def alphabetizer(input_list: list[str]) -> dict[str, list[str]]:
+    """Returns a dictionary with unique alphabet letters as keys and lists of words that start with each letter as values."""
+    final_dict: dict = {}
+    for word in input_list:
+        first_letter = word[0].lower()
+        if first_letter in final_dict:
+            final_dict[first_letter].append(word)
+        else:
+            final_dict[first_letter] = [word]
+    return final_dict
+
+
+def update_attendance(attendance_dict: [str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Updates and returns a dictionary with attendance information for a given day of the week and student."""
+    if day in attendance_dict:
+        attendance_dict[day].append(student)
+    else:
+        attendance_dict[day] = [student]
+    return attendance_dict
